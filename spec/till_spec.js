e63ad3ec886bfc2d1d1872fa0a21fb518d8dcdcd
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-07-19 15:21:46 +0100

    Add process_order method to till

--- spec/till_spec.js
+++ spec/till_spec.js
@@ -27,4 +27,11 @@ describe("Till", () => {
   it("Returns an empty basket", () => {
     expect(till.basket.length).toEqual(0);
   });
+
+  describe("#process_order", () => {
+    it("Adds a Tea to the basket", () => {
+      till.process_order('Tea', 1);
+      expect(till.basket).toEqual([ { 'item': 'Tea', 'amount': 1, 'price': 3.65 } ])
+    });
+  });
 });
