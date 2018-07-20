c8814d2eb1ec18fb390eb2addd49a809e45f5e3c
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-07-20 14:48:38 +0100

    Change tests to reflect class extraction

--- spec/till_spec.js
+++ spec/till_spec.js
@@ -3,38 +3,28 @@ describe("Till", () => {
 
   beforeEach(() => {
     printer = jasmine.createSpyObj('printer', {'print_receipt': "Tea 1 X 3.65\nTotal: $3.65"})
-    till = new Till(printer);
-  });
-
-  it("Returns the shop name", () => {
-    expect(till.shopName).toEqual("The Coffee Connection")
-  });
-
-  it("Returns the address of the shop", () => {
-    expect(till.shopAddress).toEqual("123 Lakeside Way")
-  });
-
-  it("Returns the phone number of the shop", () => {
-    expect(till.shopPhoneNumber).toEqual("16503600708")
-  });
-
-  it("Returns the menu", () => {
-    expect(till.menu).toEqual(({ 'Cafe Latte': 4.75,
-                                 'Flat White': 4.75,
-                                 'Cappucino': 3.85,
-                                 'Single Espresso': 2.05,
-                                 'Double Espresso': 3.75,
-                                 'Americano': 3.75,
-                                 'Cortado': 4.55,
-                                 'Tea': 3.65,
-                                 'Choc Mudcake': 6.4,
-                                 'Choc Mousse': 8.2,
-                                 'Affogato': 14.8,
-                                 'Tiramisu': 11.4,
-                                 'Blueberry Muffin': 4.05,
-                                 'Chocolate Chip Muffin': 4.05,
-                                 'Muffin Of The Day': 4.55
-                               }));
+    class Shop {
+      constructor() {
+        this.menu = ({ 'Cafe Latte': 4.75,
+                       'Flat White': 4.75,
+                       'Cappucino': 3.85,
+                       'Single Espresso': 2.05,
+                       'Double Espresso': 3.75,
+                       'Americano': 3.75,
+                       'Cortado': 4.55,
+                       'Tea': 3.65,
+                       'Choc Mudcake': 6.4,
+                       'Choc Mousse': 8.2,
+                       'Affogato': 14.8,
+                       'Tiramisu': 11.4,
+                       'Blueberry Muffin': 4.05,
+                       'Chocolate Chip Muffin': 4.05,
+                       'Muffin Of The Day': 4.55
+                     })
+      }
+    }
+    shop = new Shop();
+    till = new Till(shop, printer);
   });
 
   it("Returns an empty basket", () => {
