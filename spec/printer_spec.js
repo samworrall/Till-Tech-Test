1533e45c3a563740f291f86dad68da5096144f12
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-07-21 21:08:58 +0100

    Throws error if cash is less than total

--- spec/printer_spec.js
+++ spec/printer_spec.js
@@ -26,4 +26,10 @@ describe("Printer", () => {
       expect(printer.print_receipt(fakeShop, log, customer, 20.00)).toEqual("The Coffee Connection\n123 Lakeside Way\nPhone: 16503600708\nSam\nTea 1 X 3.65\nAmericano 2 X 3.75\nTax: $0.96\nTotal: $12.11\nCash: $20\nChange: $7.89")
     });
   });
+
+  describe("#check_for_valid_cash", () => {
+    it("Throws an error if cash is less than total", () => {
+      expect( () => { printer.check_for_valid_cash(5.50, 1.50) } ).toThrow("Total exceeds cash given")
+    });
+  });
 });
